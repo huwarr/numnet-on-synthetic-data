ae706bd8fb8a609a115342044f7a6c3b6a16b2c2
huwarr <mikhalmatova@gmail.com>
2022-04-22 00:06:15 +0300

    modified setup script

--- setup.sh
+++ setup.sh
@@ -3,28 +3,50 @@ TMSPAN=$1
 # Requirements
 pip install -r requirements.txt
 
-# Download drop data
-wget -O drop_dataset.zip https://s3-us-west-2.amazonaws.com/allennlp/datasets/drop/drop_dataset.zip
-unzip drop_dataset.zip
+# Download synthetic data, already converted to DROP format
+mkdir synthetic_data && cd synthetic_data
+pip install gdown
+# Numeric
+gdown https://drive.google.com/uc?id=1WoCuawj3F1RRHG9RJ0Pfow597ASgsTf5
+gdown https://drive.google.com/uc?id=1juJczB0mQorhKOpfvE6z0_hFxP44jkUd
+# Textual
+gdown https://drive.google.com/uc?id=1TXZv_za1I_zC3LZ4bg2A8IJmOLpqd7xo
+gdown https://drive.google.com/uc?id=1p3OeXhpmbdrhba4P_onWveUVlPinyTig
 
-# Download GenBERT pretrained model
-cd drop_dataset
-pip install --upgrade --no-cache-dir gdown
-gdown --folder https://drive.google.com/drive/folders/1-KmhWF4Jex4gyuz1J2VANd1ycmtsggQ3
-cd ..
+# Download RoBERTa
+mkdir roberta.large && cd roberta.large
+wget -O pytorch_model.bin https://s3.amazonaws.com/models.huggingface.co/bert/roberta-large-pytorch_model.bin
+wget -O config.json https://s3.amazonaws.com/models.huggingface.co/bert/roberta-large-config.json
+'''
+Modify config.json from "output_hidden_states": false to "output_hidden_states": true
+'''
+wget -O vocab.json https://s3.amazonaws.com/models.huggingface.co/bert/roberta-large-vocab.json
+wget -O merges.txt https://s3.amazonaws.com/models.huggingface.co/bert/roberta-large-merges.txt
+
+cd ../..
 
 # Tag based multi-span extraction -- NumNet+ v2
 if [ ${TMSPAN} = tag_mspan ]; then
     # Train
-    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 tag_mspan drop_dataset
+    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 tag_mspan synthetic_data
     # Eval
-    sh eval.sh drop_dataset/drop_dataset_dev.json prediction.json tag_mspan numnet_plus_345_LR_5e-4_BLR_1.5e-5_WD_5e-5_BWD_0.01tag_mspan/checkpoint_best.pt drop_dataset/genbert
-    python3 drop_eval.py --gold_path drop_dataset/drop_dataset_dev.json --prediction_path prediction.json
+    sh eval.sh synthetic_data/synthetic_numeric_dev_drop_format.json synthetic_data/synthetic_textual_mixed_min3_max6_up0.7_dev.json prediction_num.json prediction_text.json tag_mspan numnet_plus_345_LR_5e-4_BLR_1.5e-5_WD_5e-5_BWD_0.01tag_mspan/checkpoint_best.pt synthetic_data/roberta.large
+    # Numeric
+    echo "Evaluating numeric data..."
+    python drop_eval.py --gold_path synthetic_data/synthetic_numeric_dev_drop_format.json --prediction_path prediction_num.json
+    # Textual
+    echo "Evaluating textual data..."
+    python drop_eval.py --gold_path synthetic_data/synthetic_textual_mixed_min3_max6_up0.7_dev.json --prediction_path prediction_text.json
 # Simple multi-span extraction -- NumNet+
 else
     # Train
-    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 no drop_dataset
+    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 no synthetic_data
     # Eval
-    sh eval.sh drop_dataset/drop_dataset_dev.json prediction.json no numnet_plus_345_LR_5e-4_BLR_1.5e-5_WD_5e-5_BWD_0.01/checkpoint_best.pt drop_dataset/genbert
-    python3 drop_eval.py --gold_path drop_dataset/drop_dataset_dev.json --prediction_path prediction.json
+    sh eval.sh synthetic_data/synthetic_numeric_dev_drop_format.json synthetic_data/synthetic_textual_mixed_min3_max6_up0.7_dev.json prediction_num.json prediction_text.json no numnet_plus_345_LR_5e-4_BLR_1.5e-5_WD_5e-5_BWD_0.01/checkpoint_best.pt synthetic_data/roberta.large
+    # Numeric
+    echo "Evaluating numeric data..."
+    python drop_eval.py --gold_path synthetic_data/synthetic_numeric_dev_drop_format.json --prediction_path prediction_num.json
+    # Textual
+    echo "Evaluating textual data..."
+    python drop_eval.py --gold_path synthetic_data/synthetic_textual_mixed_min3_max6_up0.7_dev.json --prediction_path prediction_text.json
 fi
