fb11f9123305b5c6d9353ad65c1d2c349684d6ac
huwarr <mikhalmatova@gmail.com>
2022-04-28 20:27:56 +0300

    added path to roberta

--- prepare_roberta_data.py
+++ prepare_roberta_data.py
@@ -8,6 +8,7 @@ from tag_mspan_robert_gcn.drop_roberta_mspan_dataset import DropReader as TDropR
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_type", type=str)
 parser.add_argument("--input_path", type=str)
+parser.add_argument("--roberta_path", type=str)
 parser.add_argument("--output_dir", type=str)
 parser.add_argument("--passage_length_limit", type=int, default=463)
 parser.add_argument("--question_length_limit", type=int, default=46)
@@ -15,7 +16,7 @@ parser.add_argument("--tag_mspan", action="store_true")
 
 args = parser.parse_args()
 
-tokenizer = RobertaTokenizer.from_pretrained(args.input_path + "/roberta.large")
+tokenizer = RobertaTokenizer.from_pretrained(args.roberta_path)
 
 if args.tag_mspan:
     dev_reader = TDropReader(
